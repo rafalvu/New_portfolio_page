c79828f37952afa11bd80fd156b74659b66d684e
wooczak <kacper.luczak99@gmail.com>
2024-11-14 12:51:15 +0100

    Updating toggling logic

--- app.js
+++ app.js
@@ -3,6 +3,26 @@ const nav = document.getElementById("nav-sidebar");
 const navElementAbout = document.querySelectorAll('a[href="#about"]');
 const navElementPortfolio = document.querySelectorAll('a[href="#portfolio"]');
 
+const ITOpenLabel = document.getElementById("it-open-label");
+const audioOpenLabel = document.getElementById("audio-open-label");
+const ITPanel = document.getElementById("it-panel");
+const audioPanel = document.getElementById("audio-panel");
+
+ITOpenLabel.addEventListener("click", function () {
+  audioPanel.classList.remove("active");
+  ITPanel.classList.contains("active")
+    ? null
+    : ITPanel.classList.add("active");
+});
+
+audioOpenLabel.addEventListener("click", function () {
+  ITPanel.classList.remove("active");
+
+  audioPanel.classList.contains("active")
+    ? null
+    : audioPanel.classList.add("active");
+});
+
 // Toggle sidebar visibility when hamburger icon is clicked
 hamburger.addEventListener("click", function () {
   nav.classList.toggle("active");
@@ -33,7 +53,7 @@ navElementPortfolio.forEach((element) => {
   });
 });
 
-document.addEventListener("DOMContentLoaded", function() {
+document.addEventListener("DOMContentLoaded", function () {
   document.getElementById("one").checked = true;
   document.getElementById("three").checked = true;
 });
